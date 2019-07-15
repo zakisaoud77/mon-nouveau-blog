416af4aac240e15f065a13b83342d7bd88d9fb4a
zsaoud <zakaria.saoud@live.fr>
2019-07-15 11:20:14 +0200

    Mon app Django Girls, premier commit

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\zsaoudlo\desktop\django_small_project\djangogirls\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
